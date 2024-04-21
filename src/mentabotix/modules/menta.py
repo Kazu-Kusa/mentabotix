1d9fb5eaef8c24ecddcd469dc4b03d22c24cbe7f
Whth <88489697+Whth@users.noreply.github.com>
2024-04-21 22:09:58 +0800

    feat(menta): updater constructor

--- src/mentabotix/modules/menta.py
+++ src/mentabotix/modules/menta.py
@@ -1,45 +1,52 @@
-from ctypes import Array
 from enum import Enum
 from inspect import signature, Signature
-from typing import Callable, TypeAlias, SupportsIndex, List, Optional, Tuple, Sequence, NamedTuple, Union, Self, TypeVar
+from typing import (
+    Callable,
+    TypeAlias,
+    List,
+    Optional,
+    Tuple,
+    Sequence,
+    NamedTuple,
+    Union,
+    Self,
+    TypeVar,
+    SupportsInt,
+    SupportsFloat,
+)
 
 from .exceptions import BadSignatureError, RequirementError, SamplerTypeError
 
-SensorData = TypeVar("SensorData")
+SensorData = TypeVar("SensorData", float, int)
 # basically, no restrictions, support py objects or ctypes._CData variants
-SensorDataSequence: TypeAlias = Sequence[SensorData] | Array
+SensorDataSequence: TypeAlias = Sequence[SensorData]
+UpdaterClosure = Callable[[], SensorDataSequence] | Callable[[], SensorData]
 
 SequenceSampler: TypeAlias = Callable[[], SensorDataSequence]
-IndexedSampler: TypeAlias = Callable[[SupportsIndex], SensorData]
+IndexedSampler: TypeAlias = Callable[[int], SensorData]
 DirectSampler: TypeAlias = Callable[[], SensorData]
 
 Sampler: TypeAlias = Union[SequenceSampler, IndexedSampler, DirectSampler]
 
-PY_OBJ_BASELINE = 0
-C_OBJ_BASELINE = 100
-
 
 class SamplerType(Enum):
-    SEQ_SAMPLER: int = 1 + PY_OBJ_BASELINE
-    IDX_SAMPLER: int = 2 + PY_OBJ_BASELINE
-    DRC_SAMPLER: int = 3 + PY_OBJ_BASELINE
-    C_SEQ_SAMPLER: int = 1 + C_OBJ_BASELINE
-    C_IDX_SAMPLER: int = 2 + C_OBJ_BASELINE
-    C_DRC_SAMPLER: int = 3 + C_OBJ_BASELINE
+    """
+    采样器类型
+    """
+
+    SEQ_SAMPLER: int = 1
+    IDX_SAMPLER: int = 2
+    DRC_SAMPLER: int = 3
 
 
 class SamplerUsage(NamedTuple):
-    used_sampler_index: SupportsIndex
-    required_data_indexes: Sequence[SupportsIndex]
+    used_sampler_index: int
+    required_data_indexes: Sequence[int]
 
 
 class Menta:
-    __supported_py_obj_identifier__ = (
-        "__float__",
-        "__int__",
-        "__bytes__",
-    )  # reserverd to check the return type of the sampler
-    __supported_c_obj_identifier__ = ("value",)
+    # reserved to check the return type of the sampler
+    __supported__ = (SupportsInt, SupportsFloat)
 
     def __init__(
         self,
@@ -64,32 +71,15 @@ class Menta:
             sig = signature(sampler)  # 获取采样器的签名
 
             match sig.return_annotation:  # 根据采样器的返回类型进行匹配
-                case seq_sampler_ret if isinstance(seq_sampler_ret, Sequence) and len(sig.parameters) == 0:
+                case seq_sampler_ret if issubclass(seq_sampler_ret, Sequence) and len(sig.parameters) == 0:
                     # 如果返回类型是序列且无参数，则认定为序列采样器
                     self.sampler_types.append(SamplerType.SEQ_SAMPLER)
-                case c_seq_sampler_ret if isinstance(c_seq_sampler_ret, Array) and len(sig.parameters) == 0:
-                    # 如果返回类型是ctypes._CData且无参数，则认定为序列采样器
-                    self.sampler_types.append(SamplerType.C_SEQ_SAMPLER)
-                case idx_sampler_ret if len(sig.parameters) == 1 and (
-                    any(hasattr(idx_sampler_ret, ret_type) for ret_type in self.__supported_py_obj_identifier__)
-                ):
-                    # 如果有一个参数且其返回类型在支持的类型列表中，则认定为索引采样器
+                case idx_sampler_ret if len(sig.parameters) == 1 and isinstance(idx_sampler_ret, self.__supported__):
+                    # 如果返回类型是索引且只有一个参数，则认定为索引采样器
                     self.sampler_types.append(SamplerType.IDX_SAMPLER)
-                case c_idx_sampler_ret if len(sig.parameters) == 1 and (
-                    any(hasattr(c_idx_sampler_ret, ret_type) for ret_type in self.__supported_c_obj_identifier__)
-                ):
-                    # 如果有一个参数且其返回类型在支持的类型列表中，则认定为索引采样器
-                    self.sampler_types.append(SamplerType.C_IDX_SAMPLER)
-                case drc_sampler_ret if len(sig.parameters) == 0 and (
-                    any(hasattr(drc_sampler_ret, ret_type) for ret_type in self.__supported_py_obj_identifier__)
-                ):
-                    # 如果无参数且其返回类型在支持的类型列表中，则认定为直接响应采样器
+                case drc_sampler_ret if len(sig.parameters) == 0 and isinstance(drc_sampler_ret, self.__supported__):
+                    # 如果返回类型是直接响应且无参数，则认定为直接响应采样器
                     self.sampler_types.append(SamplerType.DRC_SAMPLER)
-                case c_drc_sampler_ret if len(sig.parameters) == 0 and (
-                    any(hasattr(c_drc_sampler_ret, ret_type) for ret_type in self.__supported_c_obj_identifier__)
-                ):
-                    # 如果无参数且其返回类型在支持的类型列表中，则认定为直接响应采样器
-                    self.sampler_types.append(SamplerType.C_DRC_SAMPLER)
                 case Signature.empty:
                     # 如果采样器没有指定返回类型，则抛出异常
                     raise BadSignatureError(
@@ -98,35 +88,49 @@ class Menta:
                 case invalid_sampler_ret:
                     # 如果采样器的返回类型注解无效，则抛出异常
                     raise BadSignatureError(
-                        f"Sampler {sampler} has invalid return type annotation(s)!\bMust be {SensorDataSequence} or {SensorData} but got {invalid_sampler_ret}"
+                        f"Sampler {sampler} has invalid return type annotation(s)!\n"
+                        f"Must be {SensorDataSequence} or {SensorData} but got {invalid_sampler_ret}"
                     )
         return self  # 返回更新后的实例自身
 
     def construct_updater(self, usages: List[SamplerUsage]) -> Callable[[], Tuple]:
+        """
+        Constructs an updater function based on the given list of sampler usages.
+
+        Args:
+            usages (List[SamplerUsage]): A list of sampler usages.
+
+        Returns:
+            Callable[[], Tuple]: The constructed updater function.
+
+        Raises:
+            ValueError: If the number of sampler usages does not match the number of samplers.
+            RuntimeError: If an unsupported sampler type is encountered.
+        """
+        self.update_sampler_types()
         if len(usages) != len(self.samplers):
             raise ValueError(
                 f"Number of sampler usages ({len(usages)}) does not match number of samplers ({len(self.samplers)}), have you used the update_sampler_types() method?"
             )
-        used_samplers: List[Tuple[Sampler, SamplerType]] = []
-        for i in range(len(self.samplers)):
-            used_samplers.append((self.samplers[i], self.sampler_types[i]))
+        update_funcs: List[UpdaterClosure] = []
 
-        # TODO
-        def updater() -> Tuple:
-            return tuple(
-                [
-                    sampler()
-                    for sampler in self.samplers
-                    for usage in usages
-                    if usage.used_sampler_index == self.samplers.index(sampler)
-                ]
-            )
+        for usage in usages:
+            match self.sampler_types[usage.used_sampler_index]:
+                case SamplerType.SEQ_SAMPLER:
+                    update_funcs.append(self._resolve_seq_sampler(usage))
+                case SamplerType.IDX_SAMPLER:
+                    update_funcs.append(self._resolve_idx_sampler(usage))
+                case SamplerType.DRC_SAMPLER:
+                    update_funcs.append(self._resolve_drc_sampler(usage))
+                case _:
+                    raise RuntimeError(f"Unsupported sampler type: {self.sampler_types[usage.used_sampler_index]}")
 
-        return updater
+        def _updater() -> Tuple:
+            return tuple(update_func() for update_func in update_funcs)
 
-    def _resolve_seq_sampler(
-        self, usage: SamplerUsage
-    ) -> Callable[[], Tuple[SensorData, ...]] | Callable[[], SensorData]:
+        return _updater
+
+    def _resolve_seq_sampler(self, usage: SamplerUsage) -> UpdaterClosure:
         """
         Resolves the sampler based on the given sampler usage.
 
@@ -162,7 +166,7 @@ class Menta:
         match len(usage.required_data_indexes):
             case 0:
                 # 0 means require all data
-                return lambda: tuple(sampler())  # Callable[[], Tuple[SensorData,...]]
+                return sampler  # Callable[[],SensorDataSequence]
             case 1:
                 # 1 means require a specific data
                 unique_index = usage.required_data_indexes[0]
@@ -170,11 +174,9 @@ class Menta:
             case _:
                 # >1 means require multiple data
                 required_indexes = usage.required_data_indexes
-                return lambda: tuple(sampler()[i] for i in required_indexes)  # Callable[[], Tuple[SensorData,...]]
+                return lambda: tuple(sampler()[i] for i in required_indexes)  # Callable[[],SensorDataSequence]
 
-    def _resolve_idx_sampler(
-        self, usage: SamplerUsage
-    ) -> Callable[[], Tuple[SensorData, ...]] | Callable[[], SensorData]:
+    def _resolve_idx_sampler(self, usage: SamplerUsage) -> UpdaterClosure:
         """
         Resolves the indexed sampler based on the given sampler usage.
 
@@ -211,13 +213,38 @@ class Menta:
             case 1:
                 # 1 means require a specific data
                 unique_index = usage.required_data_indexes[0]
-                return lambda: sampler(unique_index)  # Callable[[SupportsIndex], SensorData]
+                return lambda: sampler(unique_index)  # Callable[[], SensorData]
             case _:
                 # >1 means require multiple data
                 required_indexes = usage.required_data_indexes
-                return lambda: tuple(sampler(ri) for ri in required_indexes)  # Callable[[SupportsIndex], SensorData]
+                return lambda: tuple(sampler(ri) for ri in required_indexes)  # Callable[[], SensorDataSequence]
 
-    def _resolve_drc_sampler(self, usage: SamplerUsage) -> Callable[[], SensorData]:
+    def _resolve_drc_sampler(self, usage: SamplerUsage) -> UpdaterClosure:
+        """
+        Resolves the direct sampler based on the given sampler usage.
+
+        Args:
+            usage (SamplerUsage): The sampler usage object that contains the information about the used sampler and the required data indexes.
+
+        Returns:
+            UpdaterClosure: A callable that returns a SensorData object or a tuple of SensorData objects based on the number of required data indexes.
+
+        Raises:
+            SamplerTypeError: If the sampler at the specified index is not a direct sampler.
+
+        Examples:
+            usage = SamplerUsage(used_sampler_index=0, required_data_indexes=[])
+            sampler = _resolve_drc_sampler(usage)
+            data = sampler()  # Returns a SensorData object
+
+            usage = SamplerUsage(used_sampler_index=0, required_data_indexes=[0])
+            sampler = _resolve_drc_sampler(usage)
+            data = sampler()  # Returns a SensorData object at index 0
+
+            usage = SamplerUsage(used_sampler_index=0, required_data_indexes=[0, 1, 2])
+            sampler = _resolve_drc_sampler(usage)
+            data = sampler()  # Returns a tuple of SensorData objects at indexes 0, 1, and 2
+        """
         if self.update_sampler_types().sampler_types[usage.used_sampler_index] != SamplerType.DRC_SAMPLER:
             raise SamplerTypeError(
                 f"Sampler at index {usage.used_sampler_index} is not an indexed sampler "
@@ -227,7 +254,16 @@ class Menta:
         match len(usage.required_data_indexes):
             case 0:
                 return sampler  # Callable[[], SensorData]
+            case 1:
+                # 1 means require a specific data
+                unique_index = usage.required_data_indexes[0]
+                return lambda: (sampler() << unique_index) & 1
             case _:
-                raise ValueError(
-                    f"Direct sampler does not support multiple data indexes, but got {usage.required_data_indexes}"
-                )
+                # >1 means require multiple data
+                required_indexes = usage.required_data_indexes
+
+                def _fun() -> SensorDataSequence:
+                    temp_seq = sampler()
+                    return tuple(temp_seq << ri for ri in required_indexes)
+
+                return _fun
